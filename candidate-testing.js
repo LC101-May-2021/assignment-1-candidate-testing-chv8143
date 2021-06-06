ea1cc3293510f751fe2144d5fe60e1d7e595d852
TGowen <>
2021-06-06 01:11:40 +0000

    alot

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,8 +8,14 @@ let candidateName;
 let question;
 let correctAnswer;
 let candidateAnswer;
-let questions;
-let correctAnswers;
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers=[];
 
 
@@ -17,44 +23,23 @@ function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 candidateName=(input.question('What is your name?'))
 }
-correctAnswers= ['Sally Ride',
-'true',
-'40',
-'Trajectory',
-'3']
+
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-question='Who was the first American woman in space?'
-candidateAnswer =input.question (question);
-correctAnswer= "Sally Ride";
-
-questions=['1. Who was the first American woman in space?',
-'2. True or false: 5 kilometer == 5000 meters?',
-'3. (5 + 3)/2 * 10 = ?',
-"4. Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
-'5. What is the minimum crew size for the ISS?'
-];
-
-
+// question='Who was the first American woman in space?'
+// candidateAnswer =input.question (question);
+// correctAnswer= "Sally Ride";
 
 for (let i=0; i<questions.length; i++){
-
-
-candidateAnswer= (input.question (questions[i]))
-candidateAnswers.push(candidateAnswer)
-
-
+candidateAnswer= input.question (questions[i]);
+candidateAnswers.push(candidateAnswer);
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
-
   
-{ console.log (`Your answer: ${candidateAnswer}\nCorrect answer: ${correctAnswers[i]}\n`);
+console.log (`Your answer: ${candidateAnswer}\nCorrect answer: ${correctAnswers[i]}\n`);
 }   
 }
-return candidateAnswers
-}
+
 console.log(candidateAnswers)
 
 function gradeQuiz(candidateAnswers) {
@@ -65,8 +50,8 @@ if (candidateAnswers[i].toLowerCase()===correctAnswers[i].toLowerCase()){
   rightAnswers++;
 } 
 }
-  let grade=((rightAnswers/5)*100)
-  console.log (`>>>Overall Grade: ${grade} (${rightAnswers} of 5 responses correct) <<<`)
+  let grade=(rightAnswers/questions.length)*100
+  console.log (`>>>Overall Grade: ${grade} ${rightAnswers} of ${questions.length} responses correct) <<<`)
 if (grade>=80){
   console.log ('>>> Status: PASSED <<<');
 }else 
